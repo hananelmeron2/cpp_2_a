ad17b2dcf2839d60f4cb57a6d207609b664d3e10
hananel meron <44967915+hananelmeron2@users.noreply.github.com>
2020-04-05 17:26:13 +0300

    Add files via upload

---
+++ FamilyTree.cpp
@@ -0,0 +1,44 @@
+#include "FamilyTree.hpp"
+#include <iostream>
+using namespace std;
+
+
+
+namespace family
+{
+Tree::Tree(string name)
+{
+
+};
+
+Tree Tree::addFather(string son_name, string father_name)
+{
+    return Tree("");
+};
+
+Tree Tree::addMother(string son_name, string mom_name)
+{
+    return Tree("");
+};
+
+void Tree::display()
+{
+
+};
+
+string Tree::relation(string name)
+{
+    return "";
+};
+
+string Tree::find(string name)
+{
+    return "";
+};
+
+void Tree::remove(string name)
+{
+
+};
+
+}
